b4659c6633094031dfbb80cd9610d89374244bcf
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-10-22 17:34:05 +0700

    create product repository

---
+++ repositories/product.go
@@ -0,0 +1,50 @@
+package repositories
+
+import (
+	"go-batch2/models"
+
+	"gorm.io/gorm"
+)
+
+type ProductRepository interface {
+	GetProducts() ([]models.Product, error)
+	GetProductByID(ID int) (models.Product, error)
+	CreateProduct(product models.Product) (models.Product, error)
+	UpdateProduct(product models.Product, ID int) (models.Product, error)
+	DeleteProduct(product models.Product, ID int) (models.Product, error)
+}
+
+func RepositoryProduct(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) GetProducts() ([]models.Product, error) {
+	var products []models.Product
+	err := r.db.Preload("User").Find(&products).Error
+
+	return products, err
+}
+
+func (r *repository) GetProductByID(ID int) (models.Product, error) {
+	var product models.Product
+	err := r.db.Preload("User").First(&product, ID).Error
+
+	return product, err
+}
+
+func (r *repository) CreateProduct(product models.Product) (models.Product, error) {
+	err := r.db.Create(&product).Error
+	return product, err
+}
+
+func (r *repository) UpdateProduct(Product models.Product, ID int) (models.Product, error) {
+	err := r.db.Save(&Product).Error
+
+	return Product, err
+}
+
+func (r *repository) DeleteProduct(Product models.Product, ID int) (models.Product, error) {
+	err := r.db.Delete(&Product).Error
+
+	return Product, err
+}
